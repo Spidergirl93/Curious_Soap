79a7113f73f35af6b8327667e5b7750df3783bd6
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-08-29 13:39:20 +0200

    rotate when holding left or right arrows

---
+++ Curious_Soap/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float torqueAmount = 1;
+
+    Rigidbody2D surferBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        surferBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            surferBody.AddTorque (-torqueAmount);
+        }
+    }
+}
